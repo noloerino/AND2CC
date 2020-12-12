271ecdd6759f4f35243b27502dc342d8d5e984a1
noloerino <jhshi07@gmail.com>
2020-12-11 23:48:30 -0800

    Make server capable of talking to 2 at once

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ README instructions tbd once we figure out the toolchain. Two things to note if 
 #define NRFX_SPI2_ENABLED 1
 #define SPI2_ENABLED 1
 ```
-2. To compile code for the secondary robot, run `make CFLAGS=-DSECONDARY flash`
+2. To compile code for the secondary robot, run `make CFLAGS=-DSECONDARY flash`. It may be necessary
+to `make clean` or just touch one of the source files first because the compiler's not smart enough
+to realize the files changed I guess.
--- ble_server/ble_server.py
+++ ble_server/ble_server.py
@@ -14,6 +14,8 @@ from bleak import BleakClient, BleakScanner
 DDD_SERVICE_UUID = "32e61089-2b22-4db5-a914-43ce41986c70"
 DDD_CHAR_UUID    = "32e6108a-2b22-4db5-a914-43ce41986c70"
 
+CONN_TIMEOUT = 20.0 # seconds
+
 # < means little endian
 # B means unsigned byte
 STATE_LAYOUT = "<B"
@@ -28,52 +30,71 @@ CMD_LUT = {
     "z": 6,
 }
 
+async def get_buckler_ch(buckler):
+    await buckler.connect(timeout=CONN_TIMEOUT)
+    # Get service
+    svs = await buckler.get_services()
+    sv = svs.get_service(DDD_SERVICE_UUID)
+    # Get characteristic
+    return sv.get_characteristic(DDD_CHAR_UUID).handle
+
 # https://bleak.readthedocs.io/en/latest/usage.html
 async def run():
     global led_status, l_drive, r_drive
-    addr = None
-    print("searching for buckler...")
+    print("searching for DDDs...")
     devices = await BleakScanner.discover()
+    buckler_0 = None
+    buckler_1 = None
+    task_0 = None
+    task_1 = None
     for d in devices:
         # print(f"device {d} at address {d.address}")
         if "EE149" in d.name:
-            print(f"device {d}")
-            addr = d.address
-            break
-    if addr is None:
-        print("couldn't find buckler")
+            print(f"discovered device {d}")
+            if "(0)" in d.name:
+                buckler_0 = BleakClient(d.address)
+                print(f"connecting to DDD 0 @ addr {d.address}")
+                task_0 = get_buckler_ch(buckler_0)
+                if buckler_1 is not None:
+                    break
+            elif "(1)" in d.name:
+                buckler_1 = BleakClient(d.address)
+                print(f"connecting to DDD 1 @ addr {d.address}")
+                task_1 = get_buckler_ch(buckler_1)
+                if buckler_0 is not None:
+                    break
+            else:
+                print("for some reason, could not connect to EE149 robot")
+                print("please double check the device local name")
+    if buckler_0 is None or buckler_1 is None:
+        print("couldn't find at least one DDD robot")
         return
 
-    # addr = "C2BFB6D8-9256-43F1-8E79-5CC96EEE04F6"
-
-    buckler = BleakClient(addr)
     try:
-        print(f"connecting to addr {addr}")
-        await buckler.connect(timeout=20.0)
-        print("connected")
-
-        # Get service
-        svs = await buckler.get_services()
-        sv = svs.get_service(DDD_SERVICE_UUID)
-        # Get characteristic
-        ch = sv.get_characteristic(DDD_CHAR_UUID).handle
+        print("DDDs found, verifying characteristics...")
+        chs = await asyncio.gather(task_0, task_1)
+        print("Both DDDs ready!")
 
         while True:
-            char_value = await buckler.read_gatt_char(ch)
+            char_values = await asyncio.gather(
+                buckler_0.read_gatt_char(chs[0]),
+                buckler_1.read_gatt_char(chs[1]),
+            )
             # TODO use this feedback to set state vars
-            print(f"read value: {struct.unpack(STATE_LAYOUT, char_value)}")
+            print(f"read value: {[struct.unpack(STATE_LAYOUT, v) for v in char_values]}")
             cmd = input("ddd> ").strip()
             if cmd == "quit":
                 print("quitting")
                 break
             if cmd in CMD_LUT:
-                await buckler.write_gatt_char(ch, struct.pack(
-                    STATE_LAYOUT, CMD_LUT[cmd]
-                ))
+                await asyncio.gather(
+                    buckler_0.write_gatt_char(chs[0], struct.pack(STATE_LAYOUT, CMD_LUT[cmd])),
+                    buckler_1.write_gatt_char(chs[1], struct.pack(STATE_LAYOUT, CMD_LUT[cmd])),
+                )
             else:
                 print(f"invalid command: {cmd}")
     finally:
-        await buckler.disconnect()
+        await asyncio.gather(buckler_0.disconnect(), buckler_1.disconnect())
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
